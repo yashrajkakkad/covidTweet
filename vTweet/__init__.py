52b9b8f9d5858cc85ebf956bfbaca1fb2296e4ef
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-03-24 12:17:25 +0530

    Movied auth and api objects to init

--- vTweet/__init__.py
+++ vTweet/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from decouple import config
+import tweepy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vtweet:vtweet!#%@localhost/vtweet'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+auth = tweepy.OAuthHandler(
+    config('CONSUMER_KEY'), config('CONSUMER_SECRET'))
+api = tweepy.API(auth)
 
 # from vTweet.models import Database
 # database = Database()
