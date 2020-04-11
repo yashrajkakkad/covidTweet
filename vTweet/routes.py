19bb7aafc9eb5165416d221e74f1885885216092
PrayagS <prayag.s@ahduni.edu.in>
2020-04-11 23:22:43 +0530

    Added new tables for word sentiment analysis

--- vTweet/routes.py
+++ vTweet/routes.py
@@ -3,7 +3,12 @@ from sqlalchemy import func
 from vTweet.views import insert_tweets_data, fetch_tweet_ids, insert_tweets_from_object
 from vTweet import app
 from vTweet import db, api
+<<<<<<< HEAD
 import requests
+=======
+import pickle
+from tweepy.error import TweepError
+>>>>>>> 67c6f69... Added new tables for word sentiment analysis
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,16 +53,21 @@ def home():
 def renderMap():
     return render_template('map.html')
 
-# @app.route('/fetch')
-# def fetch():
-#     tweet_ids = fetch_tweet_ids()
-#     chunks = [tweet_ids[x:x + 100] for x in range(0, len(tweet_ids), 100)]
-#     f = open('tweets.pickle', 'wb')
-#     for chunk in chunks:
-#         statuses = api.statuses_lookup(chunk, include_entities=True)
-#         for status in statuses:
-#             pickle.dump(status, f)
-#             # print(type(status))
-#             # print(status)
-#             # insert_tweets_from_object(status)
-#     return render_template_string('Hello')
+
+@app.route('/fetch')
+def fetch():
+    tweet_ids = fetch_tweet_ids()
+    chunks = [tweet_ids[x:x + 100] for x in range(0, len(tweet_ids), 100)]
+    f = open('tweets.pickle', 'wb')
+    for i, chunk in enumerate(chunks):
+        try:
+            statuses = api.statuses_lookup(chunk, include_entities=True)
+            for status in statuses:
+                pickle.dump(status, f)
+                # print(type(status))
+                # print(status)
+                # insert_tweets_from_object(status)
+        except TweepError:
+            pass
+        print(i)
+    return render_template_string('Hello')
