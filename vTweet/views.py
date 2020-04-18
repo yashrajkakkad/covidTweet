4070c6d73dae3e5b004e4e2c182ed24a8851b7af
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-04-18 14:32:19 +0530

    Better exception handling in fetch_tweet_data

--- vTweet/views.py
+++ vTweet/views.py
@@ -10,7 +10,9 @@ from datetime import datetime
 from geopy.geocoders import Nominatim
 import csv
 import pickle
+import urllib3
 from time import sleep
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -26,22 +28,23 @@ def fetch_tweet_ids(filename='corona_tweets_21.csv'):
     with open(filename) as f:
         reader = csv.reader(f)
         tweet_ids = [row[0] for row in reader]
+    tweet_ids = [j for i, j in enumerate(tweet_ids) if i % 10 == 0]
     return tweet_ids
 
 
 def fetch_tweet_data():
-    existing_tweet_ids = []
-    with open('tweets.pickle', 'rb') as f:
-        while True:
-            try:
-                status = pickle.load(f)
-                existing_tweet_ids.append(status.id)
-            except EOFError:
-                break
+    # existing_tweet_ids = []
+    # with open('tweets.pickle', 'rb') as f:
+    #     while True:
+    #         try:
+    #             status = pickle.load(f)
+    #             existing_tweet_ids.append(status.id)
+    #         except EOFError:
+    #             break
     tweet_ids = fetch_tweet_ids()
-    tweet_ids = [x for x in tweet_ids if x not in existing_tweet_ids]
+    # tweet_ids = [x for x in tweet_ids if x not in existing_tweet_ids]
     chunks = [tweet_ids[x:x + 100] for x in range(0, len(tweet_ids), 100)]
-    f = open('tweets.pickle', 'ab')
+    f = open('tweets.pickle', 'wb')
     for i, chunk in enumerate(chunks):
         print('Chunk #', i)
         try:
@@ -50,6 +53,10 @@ def fetch_tweet_data():
             print('Rate exceeded. Sleeping.....')
             sleep(15 * 60)
             statuses = api.statuses_lookup(chunk, include_entities=True)
+        except (urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError, tweepy.error.TweepError) as e:
+            sleep(60)
+            statuses = api.statuses_lookup(chunk, include_entities=True)
+
         for status in statuses:
             pickle.dump(status, f)
             # print(type(status))
