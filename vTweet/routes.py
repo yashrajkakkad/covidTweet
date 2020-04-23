faf1869778beb3272cf7b0c22a1231c2f7296004
Yashraj Kakkad <yashrajkakkad@gmail.com>
2020-04-23 07:13:52 +0530

    Fixed proper adding of countries

--- vTweet/routes.py
+++ vTweet/routes.py
@@ -191,8 +191,8 @@ def insert():
             if len(status) == 0:
                 raise Exception('No tweet')
             try:
-                # print(type(status))
-                # print(status)
+                print(type(status))
+                print(status)
                 insert_tweets_from_object(status[0])
                 green_message = 'Tweet inserted successfully'
             except Exception:
@@ -204,7 +204,8 @@ def insert():
 
 @app.route('/sentiment')
 def sentiment():
-    db.session.execute('BEGIN; DELETE FROM tweet_word_sentiment; DELETE FROM tweet_word; COMMIT;')
+    db.session.execute(
+        'BEGIN; DELETE FROM tweet_word_sentiment; DELETE FROM tweet_word; COMMIT;')
     # db.session.execute('')
     db.session.execute('CALL remove_special_characters();')
     db.session.execute('CALL calculate_word_score();')
